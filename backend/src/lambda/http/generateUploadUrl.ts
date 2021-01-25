de9543b2227c67ceab1f5a7c9b5fb3a7a87de30e
Marcus Holmgren <marcus.holmgren1@gmail.com>
2021-01-25 21:53:54 +0100

    feat: upload image with signed url
    Got the CORS issue solved with SignatureDoesNotMatch 😅

--- backend/src/lambda/http/generateUploadUrl.ts
+++ backend/src/lambda/http/generateUploadUrl.ts
@@ -1,32 +1,64 @@
 import 'source-map-support/register'
-// @ts-ignore
 import {DynamoDB, S3} from 'aws-sdk'
-import { APIGatewayProxyEvent, APIGatewayProxyResult, APIGatewayProxyHandler } from 'aws-lambda'
+import {APIGatewayProxyEvent, APIGatewayProxyResult, APIGatewayProxyHandler} from 'aws-lambda'
 import * as middy from 'middy'
-import { cors } from 'middy/middlewares'
+import {cors} from 'middy/middlewares'
+import {createLogger} from "../../utils/logger";
+
+const s3 = new S3({ signatureVersion: 'v4' })
+const dynamo = new DynamoDB.DocumentClient();
+const log = createLogger('todoless')
+const bucket = process.env.TODOS_ATTACHMENT_BUCKET
 
 export const handler: APIGatewayProxyHandler = middy(async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-  // @ts-ignore
-  const todoId = event.pathParameters.todoId
+    const todoId = event.pathParameters.todoId
 
+    const params: DynamoDB.DocumentClient.QueryInput = {
+        TableName: process.env.TODOS_DYNAMODB_TABLE,
+        KeyConditionExpression: 'todoId = :todoId',
+        ExpressionAttributeValues: {
+            ':todoId': todoId
+        }
+    }
 
-  // TODO: Return a presigned URL to upload a file for a TODO item with the provided id
-  return {
-    statusCode: 200,
-    body: JSON.stringify({uploadUrl: ''})
-  }
+    log.info(`Get signed url for todoId: ${todoId} to bucket: ${bucket}`)
+    try {
+        const item = await dynamo.query(params).promise()
+
+        if (item.Count > 0) {
+            const url = getSignedUrl(todoId)
+            // TODO: Return a presigned URL to upload a file for a TODO item with the provided id
+            return {
+                statusCode: 200,
+                body: JSON.stringify({uploadUrl: url})
+            }
+        } else {
+            return {
+                statusCode: 400,
+                body: JSON.stringify({error: 'Could not find the item with the given todoId'})
+            }
+        }
+    } catch (error) {
+        const message = `Failed to generate upload URL: ${error}`
+        log.error(message)
+        const response = {
+            statusCode: 400,
+            body: JSON.stringify({error: message})
+        }
+        return response
+    }
 }).use(cors(
-    { credentials: true }
+    {credentials: true}
     )
 )
 
-// @ts-ignore
-function getSignedUrl(bucket, key) {
-  const params = {
-    Bucket: bucket,
-    Key: key
-  }
 
-  const s3 = new S3()
-  return s3.getSignedUrl('getObject', params)
+function getSignedUrl(key: string): string {
+    const params = {
+        Bucket: bucket,
+        Key: `${key}`,
+        Expires: 300,
+    }
+
+    return s3.getSignedUrl('putObject', params)
 }
--- backend/src/lambda/http/getTodos.ts
+++ backend/src/lambda/http/getTodos.ts
@@ -1,13 +1,16 @@
 import 'source-map-support/register'
-import {DynamoDB} from 'aws-sdk'
+import {DynamoDB, S3} from 'aws-sdk'
 import {APIGatewayProxyEvent, APIGatewayProxyResult, APIGatewayProxyHandler} from 'aws-lambda'
 import * as middy from 'middy'
 import {cors} from 'middy/middlewares'
 import {createLogger} from "../../utils/logger";
 import {getUserId} from "../utils";
+import {TodoItem} from "../../models/TodoItem";
 
+const s3 = new S3({ signatureVersion: 'v4' })
 const dynamo = new DynamoDB.DocumentClient();
 const log = createLogger('todoless')
+const bucket = process.env.TODOS_ATTACHMENT_BUCKET
 
 
 export const handler: APIGatewayProxyHandler = middy(async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
@@ -24,6 +27,15 @@ export const handler: APIGatewayProxyHandler = middy(async (event: APIGatewayPro
 
     try {
         const result = await dynamo.query(params).promise();
+
+        try {
+            result.Items.forEach((item: TodoItem) => {
+                const url = getSignedUrl(item.todoId)
+                item.attachmentUrl = url;
+            })
+        } catch (e) {
+            log.error(`Failed to get signedUrl: ${e}`)
+        }
 
         const response = {
             statusCode: 200,
@@ -43,3 +55,14 @@ export const handler: APIGatewayProxyHandler = middy(async (event: APIGatewayPro
     {credentials: true}
     )
 )
+
+
+function getSignedUrl(key: string): string {
+    const params = {
+        Bucket: bucket,
+        Key: `${key}`,
+        Expires: 3600,
+    }
+
+    return s3.getSignedUrl('getObject', params)
+}
